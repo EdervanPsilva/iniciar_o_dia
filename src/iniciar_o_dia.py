cc6322f4b65eaf527d09282886f67c613acfa5a1
Edervan <eder_van@hotmail.com>
2026-03-27 07:54:38 -0300

    Adicionado pausas no codigo

--- src/iniciar_o_dia.py
+++ src/iniciar_o_dia.py
@@ -20,17 +20,20 @@ def iniciar_o_dia():
     py.sleep(1)  # Aguarda o Power BI abrir
     py.press('win')  # Pressiona a tecla Windows
     py.write('teams')  # Digita 'teams' para abrir o Microsoft Teams
+    py.sleep(1)  # Aguarda o Teams abrir
     py.press('enter')  # Pressiona Enter para abrir o Teams
 
 
     py.sleep(1)  # Aguarda o Teams abrir
     py.press('win')  # Pressiona a tecla Windows
-    py.write('outlook')  # Digita 'outlook' para abrir o Microsoft  
+    py.write('outlook')  # Digita 'outlook' para abrir o Microsoft 
+    py.sleep(1)  # Aguarda o Outlook abrir 
     py.press('enter')  # Pressiona Enter para abrir o Outlook
 
     py.sleep(1)  # Aguarda o Outlook abrir
     py.press('win')  # Pressiona a tecla Windows
     py.write('whatsapp')  # Digita 'whatsapp' para abrir o WhatsApp
+    py.sleep(1)  # Aguarda o WhatsApp abrir
     py.press('enter')  # Pressiona Enter para abrir o WhatsApp 
 
     py.sleep(1)  # Aguarda o WhatsApp abrir
